dc1a742d4af5302999012ac8f22303d44b303746
zwyz <83980260+zwyz@users.noreply.github.com>
2026-09-08 03:58:55 -0400

    name map_setkeeplods command

--- src/main/java/rs3/CommandUpdater.java
+++ src/main/java/rs3/CommandUpdater.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.stream.Collectors;
 
 public class CommandUpdater {
-    private static final int VERSION = 949;
+    private static final int VERSION = 950;
     private static final int MODE = 1;
     private static final boolean MANGLED = true;
 
